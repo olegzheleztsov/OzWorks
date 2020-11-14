df767ccce948fc81510ddc4b439fc3377f826f02
Oleg Zheleztsov <olegzheleztsov@gmail.com>
2020-11-14 12:06:46 +0200

    authentication

--- LinksShare/Controllers/BooksController.cs
+++ LinksShare/Controllers/BooksController.cs
@@ -1,23 +1,51 @@
 ﻿using LinksShare.Models;
 using LinksShare.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
 namespace LinksShare.Controllers
 {
+    [Authorize]
     [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
     {
         private readonly BookService _bookService;
+        private readonly ILogger<BooksController> _logger;
 
-        public BooksController(BookService bookService)
+        public BooksController(BookService bookService, ILogger<BooksController> logger)
         {
             _bookService = bookService;
+            _logger = logger;
         }
 
         [HttpGet]
         public ActionResult<List<Book>> Get() => _bookService.Get();
+
+        [HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("user")]
+        public IActionResult GetUser()
+        {
+            if(User.Identity != null)
+            {
+                _logger.LogInformation("Identity:");
+                _logger.LogInformation($"Name: {User.Identity.Name}, Is Authenticated: {User.Identity.IsAuthenticated}, Authentication type: {User.Identity.AuthenticationType}");
+               
+            }
+            if(User.Claims != null)
+            {
+                _logger.LogInformation("Claims:");
+                foreach(var claim in User.Claims)
+                {
+                    _logger.LogInformation($"Value: {claim.Value}, Type: {claim.Type}, Issuer: {claim.Issuer}");
+                }
+            }
+            return NoContent();
+        }
+
+
 
         [HttpGet("{id:length(24)}", Name = "GetBook")]
         public ActionResult<Book> Get(string id)
